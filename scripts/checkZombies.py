7b54e689e9bc00a3c4e06f2f170d388d78874108
cippy <marco.cipriani@cern.ch>
2020-12-09 12:03:16 +0100

    more pedantic check on bad files

--- scripts/checkZombies.py
+++ scripts/checkZombies.py
@@ -9,15 +9,19 @@ allfiles = [i for i in os.listdir(d) if '.root' in i]
 print('checking', len(allfiles), 'files')
 
 faultyfiles = []
+nTot = len(allfiles)
 
-for f in allfiles:
+for i,f in enumerate(allfiles):
     
     rf = ROOT.TFile(os.path.join(d,f),'read')
-    if rf.IsZombie():
+    if rf.IsZombie() or rf.TestBit(ROOT.TFile.kRecovered) or rf.GetListOfKeys().GetSize()==0:
         faultyfiles.append(d+f)
+    if rf.IsOpen():
+        rf.Close()
+    sys.stdout.write('>>> File {num}/{tot}   \r'.format(num=i+1,tot=nTot))
+    sys.stdout.flush()
 
-    rf.Close()
-
+print "\n"
 print('faulty files:')
 print('================')
 for i in faultyfiles:
